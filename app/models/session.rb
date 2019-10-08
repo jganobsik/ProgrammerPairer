721a7b92cd70ed2bda85e3fa105f9933681cbe2f
Joe Ganobsik <jganobsik@gmail.com>
2019-10-08 14:00:00 -0400

    instance variables+methods for participants+teams

--- app/models/session.rb
+++ app/models/session.rb
@@ -3,43 +3,57 @@
 class Session < ApplicationRecord
   has_and_belongs_to_many :users
   has_and_belongs_to_many :organizers
+  after_initialize :set_arrays
+
+  attr_accessor :participants, :teams
+
+  def set_arrays
+    @participants = []
+    @teams = []
+  end
 
   def finalize_participants
     Users.each do |user|
-      participants << user
+      @participants << user
     end
   end
 
   def assign_partners
+    until @participants.length.zero?
 
+    end
   end
 
+  def finalize_team(user1, user2)
+    teams << [user1, user2]
+    participants.delete(user1, user2)
+  end 
+
   def assign_unknown_partners
-    participants.each do |participant|
-      while participants.include?(participant)
+    @participants.each do |participant|
+      @evaluated_partners = []
+      while @participants.include?(participant) 
         @potential_partner = participants.sample
-        if participant.evaluate_partner(@potential_partner) == 0
-            teams << [participant, @potential_partner]
-            participants.delete(participant)
-            participants.delete(@potential_partner)
-          else  
-            @potential_partner = participants.sample
+        # make team if user has not listed the potential partner as a friend
+        if participant.evaluate_partner(@potential_partner) < 2
+          finalize_team(participant, @potential_partner)
+        # make team of friends if all other eligible participants have been evaluated
+        elsif @evaluated_partners.length == @participants.length - 2
+          finalize_team(participant, @potential_partner)
+        else 
+          @evaluated_partners << @potential_partner
+          @potential_partner = @participants.except(@evaluated_partners).sample
         end
       end
     end
   end
 
   def make_uneven_team
-    @first_user = participants.sample
-    @second_user = participants.sample
-    @third_user = participants.sample
-
-    if @first_user == @second_user
-      @second_user = participants.sample
-    elsif @second_user == @third_user
-      @third_user = participants.sample
-    elsif @first_user == @third_user
-      @third_user = participants.sample
+    @users = @participants.sample(3)
+    teams << [@users]
+    @users.each do |user|
+    @participants.delete(user)
     end
   end
+
 end
