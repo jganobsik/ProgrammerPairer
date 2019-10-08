f3f481747b4ffd9c29fa2763dbf264e4f49bef3a
Joe Ganobsik <jganobsik@gmail.com>
2019-10-08 16:50:33 -0400

    lookup friends and partners by id

--- app/models/session.rb
+++ app/models/session.rb
@@ -13,8 +13,8 @@ class Session < ApplicationRecord
   end
 
   def add_participant(user)
-      @participants << user
-      users << user
+    @participants << user
+    users << user
   end
 
   def assign_partners
@@ -26,40 +26,51 @@ class Session < ApplicationRecord
 
   def finalize_team(user1, user2)
     teams << [user1, user2]
-    participants.delete(user1)
-    participants.delete(user2)
+    add_partners([user1, user2])
+    delete_from_participants([user1, user2])
   end
 
   def add_partners(user_array)
-    user_array.each do |u|
-      u.add_past_partner()
+    if user_array.length == 2
+      user_array[1].add_past_partner(user_array[2])
+      user_array[2].add_past_partner(user_array[1])
+    elsif user_array.length == 3
+      user_array[1].add_past_partner(user_array[2])
+      user_array[1].add_past_partner(user_array[3])
+      user_array[2].add_past_partner(user_array[1])
+      user_array[2].add_past_partner(user_array[3])
+      user_array[3].add_past_partner(user_array[1])
+      user_array[3].add_past_partner(user_array[2])
     end
   end
 
   def delete_from_participants(user_array)
-  user_array.each do |u|
-    @participants.delete(u)
+    user_array.each do |u|
+      @participants.delete(u)
+    end
   end
+
+  def generate_potential_partners(user_array)
+    user_array.each do |_u|
+      @participants.keep_if { |pp| !p.knows?(pp) }
+    end
   end
+
   def assign_unfamiliar_partners
-    @participants.each do |participant|
-      @potential_partners = @participants.keep_if{|p| !participant.knows(p)}
-    if !@potential_partners.zero? && @participants.length > 2
-      finalize_team(participant,  @potential_partners.sample)
-    else 
-
-    end
-  end
-  end
-  def make_uneven_team_of_strangers
-    @users = @participants.sample(3)
-    @evaluated_partners = []
-    if @users[1].knows?(@users[2]) 
-      
+    @participants.each do |p|
+      @potential_partners = @participants.keep_if { |pp| !p.knows?(pp) }
+      if !@potential_partners.zero? && @participants.length > 2
+        finalize_team(p, @potential_partners.sample)
+      end
     end
   end
 
-  def make_uneven_team
+  def make_uneven_team(_users)
+    if @users[1].knows?(@users[2]) || @users[2].knows?(@users[3]) || @users[1].knows?(@users[2])
+    end
+  end
+
+  def make_random_uneven_team
     @users = @participants.sample(3)
     teams << [@users]
     @users.each do |user|
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,18 +5,24 @@ class User < ApplicationRecord
   has_many :organizers, through: :sessions
 
   def knows?(user)
-    friends.include?(user.name)
+    friends.include?(user.id)
   end
 
   def add_friend(user)
-    friends << user.name unless knows?(user)
+    friends << user.id unless knows?(user)
   end
 
   def has_worked_with?(user)
-    past_partners.include?(user)
+    past_partners.include?(user.id)
   end
 
   def add_past_partner(user)
-    past_partners << user.name unless has_worked_with?(user)
+    past_partners << user.id unless has_worked_with?(user)
+  end
+
+  def add_multiple_past_partners(user_array)
+    user_array.each do |u|
+      past_partners << u.id unless has_worked_with?(user)
+    end
   end
 end
